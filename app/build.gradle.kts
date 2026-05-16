778cc2d74b4582c53ed7683da291d88e4f3d5273
Lucky-L1zard <sansangol777@gmail.com>
2026-05-16 16:08:09 +0300

    Room dependencies were added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
 }
